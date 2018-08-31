34207dd88124c60d8ee968f569df291b139624f2
CaioIbraim <caiofabiocosta@gmail.com>
2018-08-31 15:08:09 -0300

    first commit

--- application/controllers/Upload.php
+++ application/controllers/Upload.php
@@ -36,25 +36,64 @@ class Upload extends CI_Controller{
    }
 
 public function index(){
-
-    /*  $this->form_validation->set_rules('description', 'Descrição', 'trim|required|max_length[500]');
-      $this->form_validation->set_rules('tags', 'tags', 'trim');
-      if (!isset($_FILES['mp3_url']['name'])){
-          $this->form_validation->set_rules('mp3_url', 'Arquivo MP3', 'trim|required|max_length[200]|min_length[3]');
-      }
-
-      if (!isset($_FILES['img_url']['name'])){
-          $this->form_validation->set_rules('img_url', 'Imagem', 'trim|required');
-      }
-
-
-
-      if ($this->form_validation->run() == FALSE){*/
-       $this->data['conteudo'] = $this->parser->parse('telas/upload/form', $this->data, true);
-    /*  } */
-
+     $this->data['conteudo'] = $this->parser->parse('telas/upload/form', $this->data, true);
 $this->parser->parse('layout/blanc', $this->data);
 }
+
+
+
+public function do_upload(){
+  $target_dir = "uploads/mp3/";
+  $target_file = $target_dir . basename($_FILES["mp3_url"]["name"]);
+  $uploadOk = 1;
+  $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+  // Check if image file is a actual image or fake image
+
+
+/*
+  if(isset($_POST["submit"])) {
+      $check = getimagesize($_FILES["mp3_url"]["tmp_name"]);
+      if($check !== false) {
+          echo "File is an image - " . $check["mime"] . ".";
+          $uploadOk = 1;
+      } else {
+          echo "File is not an image.";
+          $uploadOk = 0;
+      }
+  }
+
+*/
+
+  // Check if file already exists
+  if (file_exists($target_file)) {
+      echo "Sorry, file already exists.";
+      $uploadOk = 0;
+  }
+  // Check file size
+  if ($_FILES["mp3_url"]["size"] > 50000000) {
+      echo "Sorry, your file is too large.";
+      $uploadOk = 0;
+  }
+  // Allow certain file formats
+  if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+  && $imageFileType != "gif" && $imageFileType != "mp3" ) {
+      echo "Sorry, only JPG, JPEG, PNG , GIF & mp3 files are allowed.";
+      $uploadOk = 0;
+  }
+  // Check if $uploadOk is set to 0 by an error
+  if ($uploadOk == 0) {
+      echo "Sorry, your file was not uploaded.";
+  // if everything is ok, try to upload file
+  } else {
+      if (move_uploaded_file($_FILES["mp3_url"]["tmp_name"], $target_file)) {
+          echo "The file ". basename( $_FILES["mp3_url"]["name"]). " has been uploaded.";
+      } else {
+          echo "Sorry, there was an error uploading your file.";
+      }
+  }
+}
+
+
 
 
 public function upload(){
--- application/views/telas/upload/form.php
+++ application/views/telas/upload/form.php
@@ -4,14 +4,14 @@
 
     <div class="well"> <h1>Compartilhe.</h1></div>
 <?php echo $error;?>
-   <?php echo form_open_multipart('upload/upload'); ?>
+   <?php echo form_open_multipart('upload/do_upload'); ?>
 
    <h4>Arquivo MP3</h4>
    <?php echo form_error('mp3_url'); ?>
    <input type="file" class="form-control" name="mp3_url" />
 
    <hr>
-   <div><button type="submit" class="btn btn-primary">Enviar</button></div>
+   <div><button type="submit" name="submit" class="btn btn-primary">Enviar</button></div>
 
    <?php echo form_close(); ?>
 </div>
